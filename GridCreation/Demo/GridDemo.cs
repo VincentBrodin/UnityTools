2c4b959ed52291c4bfd59a063523774ad17abea8
Vincent Brodin <vincent.brodin21@gmail.com>
2024-02-11 03:35:45 +0100

    Initial commit

---
+++ GridCreation/Demo/GridDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace UnityTools.GridCreation.Demo
+{
+    public class GridDemo
+    {
+        public void CreateGrid()
+        {
+            Grid<GridTileDemo> grid = new(10, 10);
+            grid.InitializeGrid();
+            
+            GridTileDemo tile = new(){
+                IsWalkable = true
+            };
+            grid[0, 0] = tile;
+            grid[0, 0].IsWalkable = false;
+        }
+    }
+    
+    public class GridTileDemo : GridTile
+    {
+        public bool IsWalkable;
+    }
+}

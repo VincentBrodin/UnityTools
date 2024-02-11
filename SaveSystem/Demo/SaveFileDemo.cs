202d73c3056595885f4b1ae2f70d316b9f065663
Vincent Brodin <vincent.brodin21@gmail.com>
2024-02-11 03:51:11 +0100

    Adding Files

---
+++ SaveSystem/Demo/SaveFileDemo.cs
@@ -0,0 +1,25 @@
+﻿namespace UnityTools.SaveSystem.Demo
+{
+    public class SaveFileDemo
+    {
+        public void CreateSaveFile()
+        {
+            SaveFile saveFile = new("saveFile", new[]{"Saves"}){
+                Cache ={
+                    ["test"] = new SaveFile.Data{
+                        Key = "test",
+                        Value = 5,
+                        Type = SaveFile.DataTypes.Int
+                    }
+                }
+            };
+
+            SaveFile.Data data = saveFile["test"];
+            data.Value = 10;
+            
+            saveFile["test"].Value = 15;
+            
+            saveFile.WriteToFile();
+        }
+    }
+}
